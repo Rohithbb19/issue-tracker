86b5cc3eb217af820253f1c14e3e73365a1eb367
Rohithbb19 <rohithbb19@gmail.com>
2024-06-22 12:31:47 +0530

    issue details page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -29,7 +29,9 @@ async function IssuesPage() {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>
                 {issue.title}
+                </Link>
                 <div className=" block md:hidden">
                   <IssueStatusBadge status={issue.status}/>
                   </div>
