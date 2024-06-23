d6ce6886ab8b2945688b28275dd7d2067d8f222d
Rohithbb19 <rohithbb19@gmail.com>
2024-06-23 13:24:29 +0530

    refactor issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,44 +1,30 @@
-import { prisma } from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
-import { notFound } from 'next/navigation'
-import ReactMarkdown from 'react-markdown'
-import {IssueStatusBadge} from "@/app/components"
-import {Pencil2Icon} from "@radix-ui/react-icons"
-import Link from 'next/link'
+import { prisma } from "@/prisma/client";
+import { Box, Grid } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 interface Props {
-    params: {
-        id:string
-    }
+  params: {
+    id: string;
+  };
 }
 
-async function IssueDetailPage({params}:Props) {
-    const issue = await prisma.issue.findUnique({
-        where:{id:parseInt(params.id)}
-    })
+async function IssueDetailPage({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-    if(!issue) notFound();
+  if (!issue) notFound();
   return (
-    <Grid columns={{initial:"1",md:"2"}} gap="5">
-        <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex gap="4" align="center" my="2">
-            <IssueStatusBadge status={issue.status}/>
-            <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className="prose" mt="4">
-            <ReactMarkdown>
-                {issue.description}
-            </ReactMarkdown>
-        </Card>
-        </Box>
-        <Box >
-            <Button>
-                <Pencil2Icon/>
-                <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-                </Button>
-        </Box>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <IssueDetails issue={issue}/>
+      </Box>
+      <Box>
+        <EditIssueButton issueId={issue.id}/>
+      </Box>
     </Grid>
-  )
+  );
 }
 
-export default IssueDetailPage
+export default IssueDetailPage;
