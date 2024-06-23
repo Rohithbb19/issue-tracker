2c0983be5ae881d5e2f2f56e6f71c7df8a0a34d7
Rohithbb19 <rohithbb19@gmail.com>
2024-06-23 13:53:29 +0530

    issue edit api

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { ErrorMessage, Spinner } from "@/app/components";
-import { createIssueSchema } from '@/app/validationSchemas';
+import { issueSchema } from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Issue } from "@prisma/client";
 import { Button, Callout, TextField } from '@radix-ui/themes';
@@ -18,12 +18,12 @@ const SimpleMDE =dynamic(
   {ssr:false}
 )
 
-type IssueFormData = z.infer<typeof createIssueSchema>
+type IssueFormData = z.infer<typeof issueSchema>
 
 function IssueForm({issue}: {issue? : Issue}) {
   const router = useRouter();
   const {register,control,handleSubmit,formState:{errors}} =  useForm<IssueFormData>({
-    resolver:zodResolver(createIssueSchema)
+    resolver:zodResolver(issueSchema)
   });
   const [error,setError] = useState('');
   const [isSubmitting,setIsSubmitting]= useState(false)
