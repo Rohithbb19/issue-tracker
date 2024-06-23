b62bb4b31f4d6bed1d31d40e31e97ac2259ce9ce
Rohithbb19 <rohithbb19@gmail.com>
2024-06-23 14:35:21 +0530

    login animation in lazy loading

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,14 @@
-import IssueForm from "../_components/IssueForm"
+import dynamic from "next/dynamic"
+import IssueFormSkeleton from "./loading"
+
+
+// lazy loading SimpleMDE OR Disabled SSR
+const IssueForm = dynamic (
+  ()=> import("@/app/issues/_components/IssueForm"),
+  {ssr:false,
+    loading: ()=><IssueFormSkeleton/>
+  }
+)
 
 function NewIssuePage() {
 
