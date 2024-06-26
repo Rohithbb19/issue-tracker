1eff4efc1f5e78326d61f29b9194721078f28399
Rohithbb19 <rohithbb19@gmail.com>
2024-06-26 12:10:06 +0530

    UX in deleting issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Spinner } from '@/app/components';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -8,13 +9,16 @@ import { useState } from 'react';
 
   const router = useRouter();
   const [error,setError] = useState(false)
+  const [isDeleting,setIsDeleting]=useState(false)
 
   async function deleteIssue(){
     try {
+        setIsDeleting(true)
         await axios.delete("/api/issues/"+ issueId)
         router.push("/issues")
         router.refresh();
     } catch (error) {
+        setIsDeleting(false)
         setError(true)
     }
 }
@@ -24,7 +28,10 @@ import { useState } from 'react';
     <>
     <AlertDialog.Root>
         <AlertDialog.Trigger>
-        <Button color='red'>Delete Issue</Button>
+        <Button color='red' disabled={isDeleting}>
+            Delete Issue
+            {isDeleting && <Spinner/>}
+            </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
             <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
