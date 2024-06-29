6460b290e18dd73146812805c320b117e0e47717
Rohithbb19 <rohithbb19@gmail.com>
2024-06-29 13:47:49 +0530

    user list in SelectUser

--- app/issues/[id]/selectUser.tsx
+++ app/issues/[id]/selectUser.tsx
@@ -1,19 +1,35 @@
-'use client';
-import { Select } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import { User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function SelectUser() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    getUsers();
+  }, []);
+
   return (
     <Select.Root>
-        <Select.Trigger placeholder='Assign...'/>
-        <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                <Select.Item value="1">Rohith</Select.Item>
-            </Select.Group>
-        </Select.Content>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
     </Select.Root>
-  )
+  );
 }
 
-export default SelectUser
+export default SelectUser;
