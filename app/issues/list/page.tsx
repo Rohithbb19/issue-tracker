2613bb08191926aba8d06f9ee1939778f7869438
Rohithbb19 <rohithbb19@gmail.com>
2024-06-30 16:34:10 +0530

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,9 +2,25 @@ import { prisma } from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import {IssueStatusBadge , Link} from "@/app/components"
 import IssuesAction from "./IssuesAction";
+import { Status } from "@prisma/client";
+import { log } from "console";
 
-async function IssuesPage() {
-  const issues = await prisma.issue.findMany();
+interface Props{
+  searchParams:{
+    status:Status
+  }
+}
+
+async function IssuesPage({searchParams,}:Props) {
+  const statuses = Object.values(Status)
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+  
+  
+  const issues = await prisma.issue.findMany({
+    where:{
+      status
+    }
+  });
 
   return (
     <div>
